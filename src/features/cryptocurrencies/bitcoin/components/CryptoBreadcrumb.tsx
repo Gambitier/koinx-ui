8f39f8169431850599b9f9b25d50df282f753d9c
Gambitier <akash.jadhav.cse@gmail.com>
2024-03-10 20:12:00 +0530

    add CryptoBreadcrumb

--- src/features/cryptocurrencies/bitcoin/components/CryptoBreadcrumb.tsx
+++ src/features/cryptocurrencies/bitcoin/components/CryptoBreadcrumb.tsx
@@ -1,3 +1,21 @@
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { Breadcrumbs, Link, Typography } from "@mui/material";
+
 export const CryptoBreadcrumb = () => {
-  return <div>CryptoBreadcrumb</div>;
+  return (
+    <Breadcrumbs
+      separator={
+        <>
+          <ChevronRightIcon style={{ margin: "-8px" }} />
+          <ChevronRightIcon style={{ margin: "-8px" }} />
+        </>
+      }
+      aria-label="breadcrumb"
+    >
+      <Link underline="none" href="/">
+        Cryptocurrencies
+      </Link>
+      <Typography>Bitcoin</Typography>
+    </Breadcrumbs>
+  );
 };
