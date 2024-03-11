c6e83ec94144372ddbf8e60ef6b833d6aa26f9fd
Gambitier <akash.jadhav.cse@gmail.com>
2024-03-12 04:42:32 +0530

    usetheme

--- src/features/cryptocurrencies/bitcoin/components/KeyEventsSlider.tsx
+++ src/features/cryptocurrencies/bitcoin/components/KeyEventsSlider.tsx
@@ -1,5 +1,5 @@
 import { GetStartedForFreeIcon } from "@/components/Elements";
-import { Container } from "@mui/material";
+import { Container, useTheme } from "@mui/material";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Slider, { Settings } from "react-slick";
@@ -17,11 +17,44 @@ function SliderItem({ label }: { label: string }) {
 }
 
 export const KeyEventsSlider = () => {
+  const theme = useTheme();
   const settings: Settings = {
     infinite: false,
     slidesToShow: 2,
     swipeToSlide: true,
     dots: true,
+    responsive: [
+      {
+        breakpoint: theme.breakpoints.values.xl,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: theme.breakpoints.values.lg,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: theme.breakpoints.values.md,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: theme.breakpoints.values.sm,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: theme.breakpoints.values.xs,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
 
   return (
