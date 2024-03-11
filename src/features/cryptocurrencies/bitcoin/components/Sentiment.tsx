f1d62cd386216b88440dc5c41b7440d5178ed70a
Gambitier <akash.jadhav.cse@gmail.com>
2024-03-11 20:08:48 +0530

    Sentiment part-1

--- src/features/cryptocurrencies/bitcoin/components/Sentiment.tsx
+++ src/features/cryptocurrencies/bitcoin/components/Sentiment.tsx
@@ -1,3 +1,39 @@
+import { Info } from "@mui/icons-material";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Stack,
+  Typography,
+} from "@mui/material";
+
 export const Sentiment = () => {
-  return <div>Sentiment</div>;
+  return (
+    <Card>
+      <CardHeader title={<Typography variant="h5">Sentiment</Typography>} />
+      <CardContent>
+        <Stack direction="column" spacing={2}>
+          <Stack direction="column" spacing={2}>
+            <Stack direction="row" spacing={1} alignItems="center">
+              <Typography variant="h6">Key Events</Typography>
+              <Info />
+            </Stack>
+            <Stack direction="row" spacing={1} alignItems="center">
+              <Typography variant="h6">Key Events</Typography>
+              <Info />
+            </Stack>
+          </Stack>
+          <Stack direction="column" spacing={2}>
+            <Stack direction="row" spacing={1} alignItems="center">
+              <Typography variant="h6">Analyst Estimates</Typography>
+              <Info />
+            </Stack>
+            <Stack direction="row" spacing={1} alignItems="center">
+              // TODO: display graphs here
+            </Stack>
+          </Stack>
+        </Stack>
+      </CardContent>
+    </Card>
+  );
 };
