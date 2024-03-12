be432d54223feecb1b81ed0abccbff18fd66aa87
Gambitier <akash.jadhav.cse@gmail.com>
2024-03-12 21:45:04 +0530

    update margin

--- src/components/Layout/ContentLayout.tsx
+++ src/components/Layout/ContentLayout.tsx
@@ -11,7 +11,7 @@ export const ContentLayout = ({ children, title }: ContentLayoutProps) => {
   return (
     <>
       <Head title={title} />
-      <Box sx={{ marginX: { xs: 1, sm: 4 }, marginY: { xs: 2, sm: 2 } }}>
+      <Box sx={{ marginX: { xs: 1, sm: 4 }, marginY: { sm: 2, md: 4 } }}>
         {children}
       </Box>
     </>
