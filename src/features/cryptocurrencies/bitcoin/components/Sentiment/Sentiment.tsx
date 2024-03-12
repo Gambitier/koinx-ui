382c2b4e0b9196aa8e8bde4ec9b52443eec475b9
Gambitier <akash.jadhav.cse@gmail.com>
2024-03-12 06:39:25 +0530

    refactor & add BuySellHoldIndicator

--- src/features/cryptocurrencies/bitcoin/components/Sentiment/Sentiment.tsx
+++ src/features/cryptocurrencies/bitcoin/components/Sentiment/Sentiment.tsx
@@ -1,5 +1,6 @@
 import { Info } from "@mui/icons-material";
 import {
+  Avatar,
   Card,
   CardContent,
   CardHeader,
@@ -8,7 +9,36 @@ import {
 } from "@mui/material";
 import { KeyEventsSlider } from "./KeyEventsSlider";
 
+const BuySellHoldIndicator = ({
+  action,
+}: {
+  action: string;
+  percent: number;
+}) => {
+  return (
+    <Stack direction="row" spacing={1} alignItems="center">
+      <Typography>{action}</Typography>
+      {/* TODO: add progress bar */}
+    </Stack>
+  );
+};
+
 export const Sentiment = () => {
+  const BuySellHoldIndicatorData = [
+    {
+      action: "Buy",
+      percent: 76,
+    },
+    {
+      action: "Sell",
+      percent: 76,
+    },
+    {
+      action: "Hold",
+      percent: 76,
+    },
+  ];
+
   return (
     <Card>
       <CardHeader title={<Typography variant="h5">Sentiment</Typography>} />
@@ -26,8 +56,13 @@ export const Sentiment = () => {
               <Typography variant="h6">Analyst Estimates</Typography>
               <Info />
             </Stack>
-            <Stack direction="row" spacing={1} alignItems="center">
-              // TODO: display graphs here
+            <Stack direction="row" spacing={1}>
+              <Avatar sx={{ width: 100, height: 100 }}>76%</Avatar>
+              <Stack direction="column" spacing={1}>
+                {BuySellHoldIndicatorData.map((item) => {
+                  return <BuySellHoldIndicator key={item.action} {...item} />;
+                })}
+              </Stack>
             </Stack>
           </Stack>
         </Stack>
