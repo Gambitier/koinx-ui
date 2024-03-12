256fd7d9c1a6177ef9782d4048c839f001500f98
Gambitier <akash.jadhav.cse@gmail.com>
2024-03-12 19:38:02 +0530

    fix: missing nav & margins

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -1,10 +1,14 @@
 import { BitcoinRoutes } from "@/features/cryptocurrencies";
 import { RouteObject } from "react-router-dom";
+import { MainLayoutWrapper } from "./MainLayoutWrapper";
 
 export const PublicRoutes: RouteObject[] = [
   {
     path: "/",
-    element: <BitcoinRoutes />,
+    element: <MainLayoutWrapper />,
+    children: [
+      { path: "", element: <BitcoinRoutes /> }, // Redirect /app to /app/plans
+    ],
   },
   // import { AuthRoutes } from '@/features/auth';
   //   {
